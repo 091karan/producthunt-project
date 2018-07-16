ebd95cd7b4f2673d17d8c083639d850041dc3f09
Karan Agarwal <091karan@gmail.com>
2018-07-16 13:58:38 +0530

    Project Completed

---
+++ producthunt/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from products import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('accounts/', include('accounts.urls')),
+    path('products/',include('products.urls')),
+    path('oauth/', include('social_django.urls', namespace='social')),
+
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
